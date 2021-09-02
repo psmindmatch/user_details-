d289161ccd3d8dfa152548ce426984b32df7e253
psmindmatch <ps@mindmatch.ai>
2021-09-02 17:41:12 +0530

    Chnage for job_listing_id search

--- app/services/bulk/bulk_upload.rb
+++ app/services/bulk/bulk_upload.rb
@@ -10,73 +10,57 @@ module Bulk
         file.write(uploaded_file.read)
       end
 
+      doc = File.open(Rails.root.join('public', "jobted_partner_company_medium.xml")) { |f| Nokogiri::XML(f) }
+            x = 0
+            sql = ""
+            batch_size = 1000
+            all_jobs = []
+            ids = []
+            doc.css('job').each do |node|
+                x = x + 1;
+                children = node.children
+                ids << children.css('id').inner_text
+            end
+
       table = CSV.parse(File.read(Rails.root.join('public', uploaded_file.original_filename)), headers: true)
       x = 0
       batch_size = 1000
+      all_job_listing_ids = []
+      all_ids = []
       
       while x < (table.size) 
         users = []
         all_users = []
         
         (batch_size).times do |i|
-          country = table.by_row[x][" country"]
-          cpc = table.by_row[x][" inbound_cpc"]  
-          location = table.by_row[x][" location"]
-          employment_type = table.by_row[x][" employment_type"]
-          company_id = table.by_row[x][" company_id"]
-          currency_code = table.by_row[x][" currency_code"]
-          language_code = table.by_row[x][" language_code"]
-          description = table.by_row[x][" description"]
-          source = table.by_row[x][" source"]
-          external_id = table.by_row[x][" external_id"]
-          country_code = table.by_row[x][" country_code"]
-          job_listing_id = table.by_row[x][" job_listing_id"]
-          job_post_link = table.by_row[x][" job_post_link"]
-          title = table.by_row[x][" title"]
-
-
-         
-            user = {"country" => ActiveRecord::Base.connection.quote(country), 
-              "inbound_cpc" => ActiveRecord::Base.connection.quote(cpc),
-              "location" => ActiveRecord::Base.connection.quote(location) ,
-              "employment_type" => ActiveRecord::Base.connection.quote(employment_type) ,
-              "company_id" => ActiveRecord::Base.connection.quote(company_id) ,
-              "currency_code" => ActiveRecord::Base.connection.quote(currency_code) ,
-              "language_code" => ActiveRecord::Base.connection.quote(language_code) ,
-              "description" => ActiveRecord::Base.connection.quote(description) ,
-              "source" => ActiveRecord::Base.connection.quote(source) ,
-              "external_id" => ActiveRecord::Base.connection.quote(external_id) ,
-              "country_code" => ActiveRecord::Base.connection.quote(country_code) ,
-              "job_listing_id" => ActiveRecord::Base.connection.quote(job_listing_id),
-              "job_post_link" => ActiveRecord::Base.connection.quote(job_post_link),
-              "title" => ActiveRecord::Base.connection.quote(title),
-              
-                  }
-            users.push(user)
+          job_listing_id = table.by_row[x]["job_listing_id"]
+          id = table.by_row[x]["id"]
+          
+          if job_listing_id.nil?
+            all_ids << id
+          else
+            all_job_listing_ids << job_listing_id
+          end
           x = x + 1;
           if(x >= table.size) 
             break
           end
         end
-        if(x <= table.size)
-          for user in users
-            all_users.push(<<-SQL.chomp)
-            (
-              #{user.values.join(',')}
-            )
-            SQL
-          end
-
-          if(all_users.size != 0)
-            sql = <<-SQL.chomp
-            INSERT INTO jobs (
-              country, inbound_cpc, location, employment_type, company_id, currency_code, language_code, description, source, external_id,  country_code, job_listing_id, job_post_link, title
-              ) VALUES #{all_users.join(',')}
-              SQL
-            ActiveRecord::Base.connection.execute(sql)
+      end
+      ans = []
+      ids.each do |id|
+        found = false
+        all_job_listing_ids.each do |all|
+          if all == id
+            found = true
+            break
           end
         end
+        if !found
+          ans << id
+        end
       end
+      puts ans
     end
   end
 end
