6e945a8f34a518298d3c8fc490a24ffe29b51cf1
psmindmatch <ps@mindmatch.ai>
2021-09-06 13:03:55 +0530

    Some changes

--- app/services/bulk/bulk_upload.rb
+++ app/services/bulk/bulk_upload.rb
@@ -6,21 +6,17 @@ module Bulk
 
     def process
       uploaded_file = @file
-      File.open(Rails.root.join('public', uploaded_file.original_filename), 'wb') do |file|
-        file.write(uploaded_file.read)
-      end
 
-      doc = File.open(Rails.root.join('public', "jobsangebote_partner_medium.xml")) { |f| Nokogiri::XML(f) }
-            x = 0
-            sql = ""
-            batch_size = 1000
-            all_jobs = []
-            ids = []
-            doc.css('job').each do |node|
-                x = x + 1;
-                children = node.children
-                ids << children.css('id').inner_text
-            end
+      doc = File.open(Rails.root.join('public', "jobsinnetwork_index.xml")) { |f| Nokogiri::XML(f) }
+      
+      x = 0
+      batch_size = 1000
+      ids = []
+      doc.css('job').each do |node|
+        children = node.children
+        ids <<  node.at_xpath('.//referencenumber').text # will extract cdata bloack
+        x += 1
+      end
 
       table = CSV.parse(File.read(Rails.root.join('public', uploaded_file.original_filename)), headers: true) 
       x = 0
@@ -29,9 +25,6 @@ module Bulk
       all_ids = []
       
       while x < (table.size) 
-        users = []
-        all_users = []
-        
         (batch_size).times do |i|
           job_listing_id = table.by_row[x]["job_listing_id"]
           id = table.by_row[x]["id"]
@@ -47,6 +40,16 @@ module Bulk
           end
         end
       end
+
+      #inserting new job_listing_id in all_job_listing_ids which is missing in active csv file.
+      x = 0
+      table2 = CSV.parse(File.read(Rails.root.join('public', 'new_jl_ids.csv')), headers: true) 
+      while x < (table2.size) 
+          job_listing_id = table2.by_row[x]["job_listing_id"]
+          all_job_listing_ids << job_listing_id
+          x += 1;
+      end
+
       ans = []
       ids.each do |id|
         found = false
